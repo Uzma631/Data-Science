5446fdc8bb50162596cad1da2651f3e5662eff32
uzma631 <auzma867@gmail.com>
2026-09-07 19:34:33 +0500

    Add Python introduction files

---
+++ print statements and comments.py
@@ -0,0 +1,12 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is uzma akram.")
+
+# Printing a fun fact about myself
+# print("I love playing soccer.")
+print("I love playing badminton.")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
